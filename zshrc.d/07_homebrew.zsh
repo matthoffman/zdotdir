b418c65a317da60e78f03458ea05ce755acb50a4
Matt Hoffman <>
2024-06-12 22:03:01 -0400

    Silly error in homebrew config

--- zshrc.d/07_homebrew.zsh
+++ zshrc.d/07_homebrew.zsh
@@ -1,8 +1,12 @@
 # Add homebrew to path
-if (( $+commands[brew] )); then 
+if [[ -z "$HOMEBREW_PREFIX" ]] || [[ -x "/opt/homebrew/bin/brew" ]] || [[ -x "/usr/local/bin/brew" ]]; then 
   # make sure we have Homebrew env vars
   if [[ -z "$HOMEBREW_PREFIX" ]]; then
-    source <(brew shellenv)
+  	if [[ -x "/opt/homebrew/bin/brew" ]]; then
+	  source <(/opt/homebrew/bin/brew shellenv)
+	elif [[ -x "/usr/local/bin/brew" ]]; then
+	  source <(/usr/local/bin/brew shellenv)
+	fi
   fi 
   # Add homebrew completions to $fpath
   fpath=("${HOMEBREW_PREFIX}/share/zsh/site-functions" $fpath)
